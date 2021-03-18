6109c3cf5f60252ab9e3081ece7e7999b6af9dd5
Dayn Augustson <42788758+Daugustson@users.noreply.github.com>
2021-03-17 20:05:48 -0600

    Update index.php
    Palying

--- 01_02_printing/index.php
+++ 01_02_printing/index.php
@@ -2,12 +2,14 @@
     // Single Line comment: Hello World! 
     echo 'Hello World'; 
 ?>
+<br>
 
 <?='Hello World'?>
+<br>
 
 <?php
 /* We prefer to use echo. 
     print is a little slower.
 */
-    print 'Hello World'; 
+  print 'Hello World!'; 
 ?>
