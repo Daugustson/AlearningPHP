78be2ba43efdf3f9c9d09247542e5d204e116598
Dayn Augustson <42788758+Daugustson@users.noreply.github.com>
2021-03-19 14:28:29 -0600

    Update index.php
    Using Single and Double quotes and escape char

--- 01_04_quotes/index.php
+++ 01_04_quotes/index.php
@@ -1,2 +1,28 @@
 <?php
+    $name = 'Joe';
+    echo "Hello, $name";
+    ?>
+    <br>
+    <?php
+    echo 'Hello, $name';
+    ?>
+    <br>
+    <?php
+    echo 'Hello, '. $name .' !';
+    ?>
+    <br>
+    <?php
+    echo 'Welcome to Joe\'s website.';
+    ?>
+    <br>
+    <?php
+    echo "This is line 1 \n";
+    echo "This is line 2";
+    ?>
+    <br>
+    <?php
+    
     echo "Joe's Nickname in HS was \"Joey Calzone\"";
+    
+    
+    ?>
