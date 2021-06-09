6c4b362a4d2263c874667b02c8798871faede0d8
Dayn Augustson <42788758+Daugustson@users.noreply.github.com>
2021-06-09 17:53:46 -0600

    Update start.php
    challenge project

--- 01_07_challenge/start.php
+++ 01_07_challenge/start.php
@@ -4,3 +4,19 @@ $city = "NYC";
 $movie = "Star Wars"; //Italics
 $friend = "George"; //Bold
 $candy = "Sour Patch Kids";
+?>
+
+
+
+<?php
+
+echo"<div style=text-align:center;><h1>Hello $name </h1></div>\n\n";
+
+echo "<p>So glad you can join us in $city today! You will be <b>$friend's</b> guide as you do a range of actitivies.</p>\n\n";
+
+echo "<p>You will end your day by seeing <i>$movie</i> at 7:00pm. Make sure to bring the money you’ve been provided. The movie ticket is \$12. $friend's favorite candy is $candy, so grab some of those too.</p>\n\n";
+
+echo "<p>...and of-course, \"May the Force be with You!\"</p>";
+
+
+?>
