9349e82fb7f132e3925f11aa7748ca5c4e818e23
Dayn Augustson <42788758+Daugustson@users.noreply.github.com>
2021-06-09 17:53:33 -0600

    Update index.php
    Playing will arrays

--- 01_06_arrays/index.php
+++ 01_06_arrays/index.php
@@ -7,7 +7,7 @@ $colors = array( 'red', 'blue', 'green' );
 
 print_r( $colors );
 
-echo '<p>' . $colors[2] . '</p>';
+echo '<p>' . $colors[1] . '</p>';
 
 $colors[] = $newColor;
 print_r( $colors );
@@ -23,11 +23,14 @@ $home_towns = array(
     'Dave' => 'Exton, PA',
     'Brian' => 'Grand Rapids, MI',
 );
-
+echo '<br>';
 echo '<pre>';
 print_r( $home_towns );
 echo '</pre>';
-
+echo '<br>';
+echo'<pre>';
+var_dump ( $home_towns );
+echo '</pre>';
 echo '<p> Where do you live. <br>I live in '. $home_towns['Brian'] . '</p>';
 
 echo '<br>';
@@ -83,4 +86,56 @@ $key = array_search(5465, array_column($userdb, 'uid'));
 echo '<br>';
 echo '<br>';
 echo $key;
+
+echo '<br>';
+$values = array(0, 0.0, false, '');
+echo '<pre>';
+var_dump($values);
+echo '</pre>';
+echo '<br>';
+echo '<pre>';
+print_r ($values);
+echo '</pre>';
+echo '<br>';
+$brothers1 = 
+    "Dayn=> array
+        'age'=> 34,
+        'job'=> 'teacher',
+        'state'=> 'PA',
+        
+    ";
+
+
+$brothers = array(
+    'joe'=> array(
+        'age'=> 34,
+        'job'=> 'teacher',
+        'state'=> 'PA',
+        ),
+    'Phile'=> array(
+        'age'=> 33,
+        'job'=> 'Photographer',
+        'state'=> 'NY',
+        ),
+    'Mike'=> array(
+        'age'=> 32,
+        'job'=> 'logistics',
+        'state'=> 'NY',
+        ),
+    'Rob'=> array(
+        'age'=> 29,
+        'job'=> 'manager',
+        'state'=> 'FL',
+        ),
+  
+    
+);
+echo '<br>';
+echo '<pre>';
+print_r ($brothers);
+echo '</pre>';
+echo '<br>';
+
+echo '<p>Mike is '. $brothers['Mike']['age'] . ' years old and lives in '.$brothers['Mike']['state']. '</p>';
+
 ?>
