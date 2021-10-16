5e416c2731e40a85b98a0bd95bed534ca08944fa
Dayn Augustson <42788758+Daugustson@users.noreply.github.com>
2021-10-16 10:16:22 -0600

    Update index.php
    Large commit

--- 02_05_switch/index.php
+++ 02_05_switch/index.php
@@ -1,17 +1,16 @@
 <?php
-$total = 10;
-switch ( $total ) :
+$total = 6;
+switch ( $total ): 
     case 1:
     case 2:
     case 3:
-    case 4:
         echo '<p>$total is less than 5</p>';
         break;
     default: 
         echo '<p>$total is 5 or more</p>';
 endswitch;
 
-$turtle = 'Leo';
+$turtle = 'Mike';
 $bandana = '';
 switch( $turtle ) {
     case 'Leo':
@@ -26,6 +25,21 @@ switch( $turtle ) {
     case 'Don':
         $bandana = 'purple';
         break;
+    default: 
+        echo '<p>No one listed with color</p>';
+
 }
 
 echo "<p>$bandana</p>";
+echo "<br>";
+
+$x = 0;
+
+/*$result = match($x) {
+    1 => '$x is 1',
+    2 => '$x is 2',
+    default => '$x is nether 1 nor 2',
+};*/
+
+//echo $result;
+
