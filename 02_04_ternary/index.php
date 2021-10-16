18fb38f6d462c75638067294bf0b5345ec0eebac
Dayn Augustson <42788758+Daugustson@users.noreply.github.com>
2021-10-16 10:16:59 -0600

    Update index.php
    temary

--- 02_04_ternary/index.php
+++ 02_04_ternary/index.php
@@ -1,9 +1,16 @@
 <?php
+
+
+
 $is_logged_in = true;
-$name = null;
-//echo "Welcome" . ($is_logged_in ? " back!" : "!");
-
-$name = $name ?: 'Joe';
-
+$name = 'Dayn';
+echo "Welcome" . ($is_logged_in ? " back!" : "!");
+echo "<br>";
+$message = $is_logged_in ? "welcome Back!": "Hello There"; 
+//$name = $name ?: 'Joe';
+$name = isset($name ) ? $name: 'Joe';
+echo $message;
+echo "<br>";
 
 echo $name;
+echo "<br>";
