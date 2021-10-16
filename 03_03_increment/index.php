cc9cd1a1ae877027cd2014934fcc7160b8b57493
Dayn Augustson <42788758+Daugustson@users.noreply.github.com>
2021-10-16 10:18:19 -0600

    Update index.php
    increment

--- 03_03_increment/index.php
+++ 03_03_increment/index.php
@@ -1,22 +1,22 @@
 <?php 
 $a = 5;
 //this will be 6
-echo 'The value of $a is ' . --$a . '<br/>'; 
+echo 'The value of $a is ' . $a++ . '<br/>'; 
 //This will still be 6
 echo 'Now $a is ' . $a . '<br/>'; 
 
 
-$a = 5;
+$b = 5;
 //this will still be 5
-echo 'The value of $a is ' . $a-- . '<br/>'; 
+echo 'The value of $a is ' . $b-- . '<br/>'; 
 //This will be 6
-echo 'Now $a is ' . $a . '<br/>'; 
+echo 'Now $a is ' . ++$b . '<br/>'; 
 
-$a = 'A';
-echo '$a is ' . ++$a . '<br/>';
+$c = 'A';
+echo '$a is ' . ++$c. '<br/>';
 
-$a = 1;
+$d = 2;
 
-$a /= 5;
-echo $a;
+$d /= 5;
+echo $d;
 
