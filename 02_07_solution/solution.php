fbd81d4395de10ee4898633d9b911995e6c06665
Dayn Augustson <42788758+Daugustson@users.noreply.github.com>
2021-10-16 10:17:42 -0600

    Update solution.php
    solutions

--- 02_07_solution/solution.php
+++ 02_07_solution/solution.php
@@ -1,11 +1,11 @@
 <?php
 $min = 1; 
 $max = 50;
-$guess = 100; //Change this value to test!
+$guess = ''; //Change this value to test!
 $num = 16; // Change this value to test!
 
 // If you really want to go nuts, try this:
-$num = rand($min, $max);
+$guess = rand($min, $max);
 
 if ( $guess < $min || $guess > $max ) {
     echo '<p>Your guess is out of range.</p>';
@@ -22,3 +22,5 @@ if ( $num == $guess ) {
 
 
 print_r( $num );
+echo '<br>';
+print_r( $guess );
