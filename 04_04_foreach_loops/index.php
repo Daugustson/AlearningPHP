0d922962e10fb1382f61e90f6ada4b75dc89bfa3
Dayn Augustson <42788758+Daugustson@users.noreply.github.com>
2021-10-23 21:30:43 -0600

    Update index.php
    playing with foreach loops and combining for loops to pull data from multidimensional array

--- 04_04_foreach_loops/index.php
+++ 04_04_foreach_loops/index.php
@@ -1,17 +1,52 @@
 <?php
-$colors = array('red', 'blue', 'green', 'yellow');
+$colors = array('red', 'blue', 'green', 'yellow', 'pink');
 
 foreach( $colors as $color ) {
     echo "<p>$color</p>";
 }
 
 $home_towns = array( 
-    'Joe' => 'Middletown, NY',
-    'Erin' => 'West Chester, PA',
-    'Dave' => 'Exton, PA',
-    'Brian' => 'Grand Rapids, MI',
+    'Dayn' => 'Middletown, NY',
+    'Kristen' => 'West Chester',
+    'Dan' => 'Exton, PA',
+    'Mark' => 'Grand Rapids, MI',
 );
 
-foreach( $home_towns as $name => $town ) {
-    echo "<p>$name lives in $town</p>";
+foreach( $home_towns as $name => $town) {
+    echo "<p>$name lives in $town </p>";
 }
+
+$brothers = array( 
+        'Joe' => array( 
+            'age' => 34,
+            'job' => 'teacher',
+            'state' => 'PA',
+        ),
+        'Phil' => array( 
+            'age' => 33,
+            'job' => 'photographer',
+            'state' => 'NY',
+        ),
+        'Mike' => array( 
+            'age' => 32,
+            'job' => 'logistics',
+            'state' => 'NY',
+        ),
+        'Rob' => array( 
+            'age' => 29,
+            'job' => 'manager',
+            'state' => 'FL',
+        ),
+    );
+
+$keys = array_keys($brothers);
+for($i = 0; $i < count($brothers); $i++) {
+    
+    echo $keys[$i] . "{<br>";
+    foreach($brothers[$keys[$i]] as $key => $value) {
+        echo $key . ' : ' . $value . "<br>";
+    }
+    echo '<br>';
+    }
+    
+    
